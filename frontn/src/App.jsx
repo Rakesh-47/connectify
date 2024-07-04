4954a7c80a4f6a961626e1a5078249173ad473ef
Rakesh <rakvc456@gmail.com>
2024-07-04 11:01:22 +0530

    background-fix

--- frontn/src/App.jsx
+++ frontn/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes,Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Homie from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
@@ -7,22 +7,21 @@ import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/authcontext.jsx';
 
 function App() {
-  const {authuser}=useAuthContext()
+  const { authuser } = useAuthContext();
+  
   return (
-    
-    <div className=" p-4 h-screen  items-center flex  justify-center ">
-      <Routes>
-        <Route path='/' element={authuser?<Homie/>:<Navigate to="/login"/>}/>
-        <Route path='/login' element={authuser?<Navigate to="/"/>:<Login/>}/>
-        <Route path='/signup' element={authuser?<Navigate to="/"/>:<Signup/>}/>
-      </Routes>
-      <Toaster/>
-      
+    <div className="relative h-screen">
+      <div className="background-container"></div>
+      <div className="p-4 h-full items-center flex justify-center">
+        <Routes>
+          <Route path='/' element={authuser ? <Homie /> : <Navigate to="/login" />} />
+          <Route path='/login' element={authuser ? <Navigate to="/" /> : <Login />} />
+          <Route path='/signup' element={authuser ? <Navigate to="/" /> : <Signup />} />
+        </Routes>
+        <Toaster />
+      </div>
     </div>
   );
-  
-
 }
-
 
 export default App;
