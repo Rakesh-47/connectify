bb5125324d6341395c85a02a858b86257fa111cf
Rakesh <rakvc456@gmail.com>
2024-07-09 22:54:37 +0530

    ch

--- backn/socket/socket.js
+++ backn/socket/socket.js
@@ -10,6 +10,7 @@ const io= new Server(server,{
     cors:{
         origin:["http://localhost:3000","https://connectify-dun.vercel.app/"],
         methods:["GET","POST"],
+        credentials:true
     }
 })
 
