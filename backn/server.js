5a3e8d533ff30ef17066671256ca45232a87563f
Rakesh <rakvc456@gmail.com>
2024-07-10 21:33:26 +0530

    fig

--- backn/server.js
+++ backn/server.js
@@ -10,6 +10,8 @@ import userroutes from './userroutes.js';
 import { app, server } from './socket/socket.js';
 
 dotenv.config();
+
+console.log(process.env.PORT)
 const PORT = process.env.PORT || 5000;
 
 
